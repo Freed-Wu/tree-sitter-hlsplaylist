89452b22d792c8ba8a4027c0f509dc01ba1c45ad
Wu, Zhenyu <wuzhenyu@ustc.edu>
2024-02-13 11:10:35 +0800

    :bug: Fix scm

--- tests/test___init__.py
+++ tests/test___init__.py
@@ -15,7 +15,7 @@ class Test:
         :rtype: None
         """
         with open(
-            os.path.join(os.path.dirname(__file__), "zho.hlsplaylist"), "rb"
+            os.path.join(os.path.dirname(__file__), "zho.m3u"), "rb"
         ) as f:
             text = f.read()
         tree = parser.parse(text)
